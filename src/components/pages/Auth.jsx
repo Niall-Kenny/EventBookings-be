299bd08f232c63df42c0bbdc32f8ebb312285786
Niall Kenny <niall.s.kenny@gmail.com>
2019-09-06 20:27:14 +0100

    template of create-react-app.

---
+++ src/components/pages/Auth.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Auth extends Component {
+    render() {
+        return (
+            <div>
+                the Auth page
+            </div>
+        );
+    }
+}
+
+export default Auth;
